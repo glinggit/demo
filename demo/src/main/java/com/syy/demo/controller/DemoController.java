8b3766ceae522665c36c36deced0cb0051f3fd6b
sunyy <so2whpu@163.com>
2017-10-20 10:09:50 +0800

    json

--- demo/src/main/java/com/syy/demo/controller/DemoController.java
+++ demo/src/main/java/com/syy/demo/controller/DemoController.java
@@ -1,5 +1,6 @@
 package com.syy.demo.controller;
 
+import java.io.IOException;
 import java.util.Date;
 import java.util.List;
 
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.syy.demo.entity.User;
 import com.syy.demo.service.inter.IDemoService;
 import com.syy.demo.service.inter.IUserService;
+import com.syy.demo.utils.JsonUtil;
 
 @Controller
 @RequestMapping("/demo")
@@ -21,32 +23,28 @@ public class DemoController extends ApplicationObjectSupport {
 
 	@Autowired
 	private IDemoService demoService;
-	
+
 	@Autowired
 	private IUserService userService;
 
-	
-	
-	
-
 	@RequestMapping(params = "method=index")
-	public String index(HttpServletRequest request, HttpServletResponse response) {
+	public void index(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		String demoValue = demoService.getDemoValue();
 		System.out.println(demoValue);
 		logger.info(demoValue + new Date());
 		logger.debug(demoValue + new Date());
 		logger.error(demoValue + new Date());
-		
-		
-		return "index";
+
+		JsonUtil.responseMessage(response, JsonUtil.codeSYS("200", "成功", demoValue));
 	}
-	
+
 	@RequestMapping(params = "method=getUser")
-	public String getUser(HttpServletRequest request, HttpServletResponse response) {
+	public String getUser(HttpServletRequest request,
+			HttpServletResponse response) {
 		List<User> listUser = userService.findAll();
-		
+
 		System.out.println(listUser.size());
-		
+
 		return "index";
 	}
 }
--- demo/src/main/java/com/syy/demo/utils/JsonUtil.java
+++ demo/src/main/java/com/syy/demo/utils/JsonUtil.java
@@ -5,12 +5,72 @@ import java.io.PrintWriter;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang.StringUtils;
+
 public class JsonUtil {
 
-	public static void responseMessage(HttpServletResponse response, String message) throws IOException {
+	public static void responseMessage(HttpServletResponse response,
+			String message) throws IOException {
 		response.setContentType("text/html;charset=UTF-8");
 		PrintWriter out = response.getWriter();
 		out.write(message);
+	}
+
+	/**
+	 * 返回数据拼接
+	 * 
+	 * @return
+	 */
+	public static String codeSYS(String code, String message, String data) {
+		if (StringUtils.isEmpty(data)) {
+			data = "{}";
+		}
+		return "{\"code\":\"" + code + "\",\"message\":\"" + message
+				+ "\",\"data\":" + data + "}";
+	}
+
+	/**
+	 * 业务请求正常返回数据
+	 * 
+	 * @param data
+	 * @return
+	 */
+	public static String getDataStr(String data) {
+		if (StringUtils.isEmpty(data)) {
+			data = "{}";
+		}
+		return "{\"state\":\"200\",\"msg\":\"成功\",\"data\":" + data + "}";
+	}
+
+	/**
+	 * 业务参数缺失报错
+	 * 
+	 * @param msg
+	 * @return
+	 */
+	public static String code010(String msg) {
+		return "{\"state\":\"010\",\"msg\":\"" + msg + "\",\"data\":{}}";
+	}
+
+	/**
+	 * 与业务规则不符
+	 */
+	public static String code011(String msg) {
+		return "{\"state\":\"011\",\"msg\":\"" + msg + "\",\"data\":{}}";
+	}
+
+	/**
+	 * 系统流程异常
+	 */
+	public static String code012(String msg) {
+		return "{\"state\":\"012\",\"msg\":\"" + msg + "\",\"data\":{}}";
+	}
+
+	/**
+	 * 系统业务处理超时
+	 */
+	public static String code013(String msg) {
+		return "{\"state\":\"013\",\"msg\":\"" + msg + "\",\"data\":{}}";
 	}
 
 }
