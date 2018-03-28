44a33b2a0f4b8600a9bc09c28159b202001aafb7
pp <pp@pp-PC>
2018-03-28 22:56:19 +0800

    dubbo-demo

---
+++ demo/src/main/java/com/syy/demo/test/Test.java
@@ -0,0 +1,30 @@
+package com.syy.demo.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test {
+	public void setA(Object o) {
+		System.out.println("a");
+	}
+	
+	public void setA(String o) {
+		System.out.println("b");
+	}
+	
+	public static void main(String[] args) {
+		Test t = new Test();
+		t.setA(null);
+		
+		String a;//不初始化不可以使用
+		System.out.println("a=");
+		
+		String b = "aaa";
+		System.out.println(b+=1);
+		System.out.println(b+100);
+		
+		
+		Map<String, String> map = new HashMap<String, String>();
+		
+	}
+}
